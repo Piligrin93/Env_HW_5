3b7d930555aba39c17ad7c917c108a8c85a36922
Vladimir Mokhov <mohov_vova@mail.u>
2022-06-09 00:50:13 +0300

    First Commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,11 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean result = Validator.check("ABCD123wqedqwdqwdqwwqr", "ABCD123", "ABCD");
+        if (result) {
+            System.out.println("Логин и пароль корректны");
+        } else {
+            System.out.println("Логин и/или пароль некоректны");
+        }
     }
 }
